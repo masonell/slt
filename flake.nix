c7f03943fd873756d189e57bbf3f5fa6e71ef2fc
Mason Ellington <mason.ellington.dev@protonmail.com>
2026-01-25 01:56:46 +0300

    vendor boring-sys crate

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "TLS playground";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import inputs.systems;
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            fish
+            openssl
+            tshark
+            nssTools
+            clang
+            llvmPackages.libclang
+            nasm
+          ];
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+
+          shellHook = ''
+            exec fish
+          '';
+        };
+      };
+    };
+}
